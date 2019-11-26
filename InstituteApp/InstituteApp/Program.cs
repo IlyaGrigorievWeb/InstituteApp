3867139d872a72d7cbe4dd2f9ffbe622ebcab838
Илья Григорьев <abrailia3@gmail.com>
2019-11-26 13:20:20 +0300

    dockerfile and migrations

--- InstituteApp/InstituteApp/Program.cs
+++ InstituteApp/InstituteApp/Program.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Database.Database;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace InstituteApp
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetService<DatabaseContext>();
+                db.Database.EnsureCreated();
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
